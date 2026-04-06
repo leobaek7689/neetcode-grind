5b90b40c36d38651da93a0a19190a9d6f74a742f
leobaek7689 <136149159+leobaek7689@users.noreply.github.com>
2026-04-06 15:05:11 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        // Sorted Array given in ascending order
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        int middle;
+
+
+        while(left <= right) 
+        {
+            middle = (right + left) / 2;
+            if(nums[middle] > target)
+            {
+                right = middle - 1;
+            }
+            else if(nums[middle] < target)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+
+        return -1;
+    }
+};
